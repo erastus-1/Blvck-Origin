41a057a503328b8f954cd24b85273bfa70416554
erastus <erastuskariuki15@gmail.com>
2020-12-31 16:45:51 +0300

    g

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,9 +9,9 @@ class UserModelTest(unittest.TestCase):
     def test_password_setter(self):
         self.assertTrue(self.new_user.pass_secure is not None)
 
-     def test_no_access_password(self):
-            with self.assertRaises(AttributeError):
-                self.new_user.password
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+             self.new_user.password
 
-        def test_password_verification(self):
-            self.assertTrue(self.new_user.verify_password('banana'))
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('banana'))
